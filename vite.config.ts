b88923b934ba62dd7debc0a272354c12e88264c7
bibi oyugbo <obehioyeoyugbo@gmail.com>
2025-02-14 13:40:05 +0100

    Third commit

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-    base: "/HappyVDay/",
+    // base: "/HappyVDay/",
     plugins: [
       tailwindcss(),
       react(),
